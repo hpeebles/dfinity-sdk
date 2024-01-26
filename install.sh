e342b0071586c686e8d44dda4b76b12c1797a583
hpeebles <hamishpeebles@gmail.com>
2024-01-26 13:04:47 +0000

    Deploying to public-manifest from  @ 4200a4ec8c1f660c5b44c8b2a7f554dcee392857 🚀

--- install.sh
+++ install.sh
@@ -161,7 +161,7 @@ DFX_RELEASE_ROOT="${DFX_RELEASE_ROOT:-$SDK_WEBSITE/downloads/dfx}"
 DFX_GITHUB_RELEASE_ROOT="${DFX_GITHUB_RELEASE_ROOT:-https://github.com/dfinity/sdk/releases/download}"
 DFX_MANIFEST_JSON_URL="${DFX_MANIFEST_JSON_URL:-$SDK_WEBSITE/manifest.json}"
 DFX_VERSION="${DFX_VERSION-}"
-SCRIPT_COMMIT_DESC="d39dde0611fda8f30a6796b40b1dbb13a0541597"
+SCRIPT_COMMIT_DESC="4200a4ec8c1f660c5b44c8b2a7f554dcee392857"
 get_tag_from_manifest_json() {
     cat \
         | tr -d '\n' \
